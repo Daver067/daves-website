c361535f63f7467fc21ffd3af708af9be0e21b63
Dave Diemert <davediemert_88@hotmail.com>
2025-01-23 11:03:48 -0700

    made new component for 2 blocks of text beside eachother

---
+++ src/components/multi-use-components/text_beside_more_text.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+interface TextBesideText {
+  header1: string;
+  paragraphs1: string[];
+  header2: string;
+  paragraphs2: string[];
+  reverse?: boolean;
+}
+const TextBesideText: React.FC<TextBesideText> = ({
+  header1,
+  paragraphs1,
+  header2,
+  paragraphs2,
+  reverse = false,
+}) => {
+  return (
+    <div
+      className={`text-lg box-border flex flex-col gap-20 lg:gap-0 ${
+        reverse ? "lg:flex-row-reverse" : "lg:flex-row"
+      }`}
+    >
+      <div
+        className={` w-[80%] ${
+          reverse ? "lg:ml-auto mr-0" : "  lg:mr-auto ml-0"
+        } mt-0 mb-auto lg:w-[45%] max-lg:mb-auto max-lg:mt-auto max-lg:ml-auto max-lg:mr-auto h-full`}
+      >
+        <h2 className="block text-2xl text-center underline decoration-1 underline-offset-4 pb-4">
+          {header1}
+        </h2>
+        {paragraphs1.map((p, i) => {
+          return (
+            <p key={i} className="relative leading-6 text-left py-2 text-base ">
+              {p}
+            </p>
+          );
+        })}
+      </div>
+      <div
+        className={` w-[80%] ${
+          reverse ? "lg:ml-0 mr-auto" : "  lg:mr-0 ml-auto"
+        } mt-0 mb-auto lg:w-[45%] max-lg:mb-auto max-lg:mt-auto max-lg:ml-auto max-lg:mr-auto h-full`}
+      >
+        <h2 className="block text-2xl text-center underline decoration-1 underline-offset-4 pb-4">
+          {header2}
+        </h2>
+        {paragraphs2.map((p, i) => {
+          return (
+            <p key={i} className="relative leading-6 text-left py-2 text-base ">
+              {p}
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TextBesideText;
