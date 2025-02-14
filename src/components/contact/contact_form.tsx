3fbfc4aa799f46cba453fa413ced592b7c0f30ea
Dave Diemert <davediemert_88@hotmail.com>
2025-02-14 12:03:16 -0700

    style: minor style change contact form font size

--- src/components/contact/contact_form.tsx
+++ src/components/contact/contact_form.tsx
@@ -144,7 +144,7 @@ const ContactForm: React.FC = () => {
                 onClick={(e) => {
                   handleSubmit(e);
                 }}
-                className="text-gray-200 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                className="text-gray-200 text-base bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg px-8 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
               >
                 Submit
               </button>
